614a3e9d67f41238b810ab9c7f7dde6b96f2236b
Rajeev Kumar <rajeevk@leewayhertz.com>
2026-01-05 11:44:58 +0530

    basic react-vite-tailwind scaffold

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+  <div className='text-5xl'>Hello</div>
+  )
+}
+
+export default App
